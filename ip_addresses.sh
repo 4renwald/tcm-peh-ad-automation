db4c743c39a84f969151c1bda39eafe7251deb86
4renwald <arnaud@arenwald.com>
2025-04-06 23:46:39 -0400

    Initial commit

---
+++ ip_addresses.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Update the variables below with the correct IP addresses for each machine
+
+# HYDRA-DC IP address
+HYDRA_DC_IP=''
+
+# THEPUNISHER  IP address
+THEPUNISHER_IP=''
+
+# SPIDERMAN IP address
+SPIDERMAN_IP=''
