2fb1e6bdec5499c76bd34e90ab407f2b745dc482
4renwald <106856367+4renwald@users.noreply.github.com>
2025-04-13 23:32:09 -0400

    Update ip_addresses.sh

--- ip_addresses.sh
+++ ip_addresses.sh
@@ -2,10 +2,10 @@
 # Update the variables below with the correct IP addresses for each machine
 
 # HYDRA-DC IP address
-HYDRA_DC_IP='10.0.0.241'
+HYDRA_DC_IP=''
 
 # THEPUNISHER  IP address
-THEPUNISHER_IP='10.0.0.127'
+THEPUNISHER_IP=''
 
 # SPIDERMAN IP address
-SPIDERMAN_IP='10.0.0.10'
+SPIDERMAN_IP=''
